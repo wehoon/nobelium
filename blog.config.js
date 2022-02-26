889d90d91b6de7577942b23a8580f6656c43ecf9
wehoon <wehoon@126.com>
2022-02-26 17:32:07 +0800

    modify width

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: '',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion', 'Wehoon'],
+    keywords: ['Blog', 'Website', 'Wehoon'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
