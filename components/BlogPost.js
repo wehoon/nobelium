63e666740cf50250454c64de6dd52a8cdd5820d0
wehoon <wehoon@126.com>
2022-02-26 17:57:07 +0800

    modify leading

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -16,7 +16,7 @@ const BlogPost = ({ post }) => {
             </time>
           </header>
           <main>
-            <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+            <p className="hidden md:block leading-7 text-gray-700 dark:text-gray-300">
               {post.summary}
             </p>
           </main>
